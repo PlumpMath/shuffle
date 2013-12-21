8d1320cc46841935d4b785c9bf30af90e4123ef8
Eric A Fode <ericfode@gmail.com>
2013-12-21 00:11:05 -0800

    did some clean up

--- src/shuffle/core.clj
+++ src/shuffle/core.clj
@@ -1,9 +1,6 @@
 (ns shuffle.core
   (:require  [clojure.math.numeric-tower :as math]))
 
-(defn abs [x] 
-  (if (< x 0) (- x) x))
-
 (defn lrc [cut deck]
   "remaining cards"
   (- (count deck) (* 2 cut)))
@@ -17,7 +14,7 @@
 
 (defn remaining-cards [cut deck]
   "returns the number of cards that are left out by the cut"
-  (abs (lrc cut deck)))
+  (math/abs (lrc cut deck)))
 
 (defn cut-size [cut deck]
   "returns the number of cards in each side of the cut"
@@ -44,51 +41,38 @@
 (defmethod active :even [cut deck] deck)
 
 (defmethod active :bottom [cut deck]
-   (vec ( flatten (conj 
-         (vec (take (cut-size cut deck) deck))
-         (vec (drop (remaining-cards cut deck)
-                    (drop (cut-size cut deck) deck)))))))
+  (let [cut-len (cut-size cut deck)
+        remaining-len (remaining-cards cut deck)]
+   (vec (flatten (conj 
+                 (vec (take cut-len deck))
+                 (vec (drop remaining-len deck
+                      (drop cut-len deck))))))))
+
+(defn- shuf-deck [top bottom remainder]
+   (vec (flatten (conj remainder (vec (interleave bottom top))))))
 
 (defn shuf [cut deck]
   (let [cut-count (cut-size cut deck)
         top       (take cut-count (active cut deck))  
         bottom    (drop cut-count (active cut deck))
         remainder (vec (remain cut deck))]
-        (vec 
-          (flatten 
-            (conj remainder (vec (interleave bottom top)))))))
+        (shuf-parts top bottom reminder)))
 
 (def shuf-memo (memoize shuf))
  
-(defn shuf-sort [cut init deck]
-  (loop [deck deck cnt init]
-    (if (= deck (sort deck))
-      cnt
-      (recur (shuf-memo cut deck) (inc cnt)))))
-
-(defn shuf-list [cut init deck]
-  (loop [deck deck cnt init accum (range (count deck))]
-    (if (= deck (sort deck))
-      accum 
-      (recur 
-        (shuf-memo cut deck) 
-        (inc cnt) 
-        (map (fn [coll x] (list coll x) ) accum deck)))))
-
 (defn shuf-times [cut init times deck]
- (loop [deck deck 
-        cnt init 
+ (loop [deck  deck 
+        cnt   init 
         accum (range (count deck))]
     (if (= times cnt)
       accum 
       (recur 
         (shuf-memo cut deck) 
         (inc cnt) 
-        (map (fn [coll x] (list coll x) ) accum deck)))) 
+        (map (fn [coll x] (list coll x) ) accum deck)))))
 
- )
 (defn shuf-sets [cut len times]
-(set (map  (comp set flatten)  
+  (set (map  (comp set flatten)  
        (shuf-times cut 1 times (shuf cut  (range len))))))
 
 
